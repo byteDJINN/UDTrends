eda7ea9539216ac12e1342054de2d6fb2999527f
Djimon <developerdjay@gmail.com>
2023-11-02 13:15:39 +0800

    made better

--- processor.py
+++ processor.py
@@ -1,6 +1,7 @@
 import json
+import math
 
-start = open("recent.json", "r", encoding="utf-8").read()
+start = open("2003-1.json", "r", encoding="utf-8").read()
 start = json.loads(start)
 end = open("2003-2.json", "r", encoding="utf-8").read()
 end = json.loads(end)
@@ -10,25 +11,21 @@ rank1 = {}
 # loop through words in curr 
 for i in start:
   if i["word"] not in rank0.keys():
-    rank0[i["word"]] = 0
-  elif i["thumbs_up"] > rank0[i["word"]]:
-    rank0[i["word"]] += 1
-
-top10 = sorted(rank0.items(), key=lambda x: x[1], reverse=True)[:10]
-print(top10)
-assert(False)
+    rank0[i["word"]] = i["thumbs_up"] - i["thumbs_down"]
+  elif i["thumbs_up"] - i["thumbs_down"] > rank0[i["word"]]:
+    rank0[i["word"]] = i["thumbs_up"] - i["thumbs_down"]
 
 for i in end:
   if i["word"] not in rank1.keys():
-    rank1[i["word"]] = i["thumbs_up"]
+    rank1[i["word"]] = i["thumbs_up"] - i["thumbs_down"]
   elif i["thumbs_up"] > rank1[i["word"]]:
-    rank1[i["word"]] = i["thumbs_up"]
+    rank1[i["word"]] = i["thumbs_up"] - i["thumbs_down"]
 
 final = {}
 #w give each word a final score based on its rank2 - rank 1 / (rank 1 - rank 0)
 for i in rank0:
-  if i in rank1 and (rank0[i] > 10):
-    final[i] = rank1[i] / rank0[i]
+  if i in rank1 and (rank1[i] > 1000):
+    final[i] = rank1[i] / rank0[i] + math.log(rank1[i])
 
 # get the top 10 words
 top10 = sorted(final.items(), key=lambda x: x[1], reverse=True)[:10]
--- scanner.py
+++ scanner.py
@@ -5,21 +5,26 @@ import tqdm
 
 def get(i):
   global dic
+  if i % round(i, -len(str(i))+1) == 0: # logarithmically saving - every 10, 100, 1000, 10000, etc.
+    print("SAVING")
+    with open(str(name)+".json", "w", encoding="utf-8") as f:
+      f.write(json.dumps(list(dic), indent=4, sort_keys=True))
   try:
     dic.append(requests.get(
-        f"https://api.urbandictionary.com/v0/define?defid={i}").json()["list"][0])
+        f"https://api.urbandictionary.com/v0/define?defid={end-i}").json()["list"][0])
   except:
     pass
 
 
-end = 17967225
-start = end - 100000
+# most recent word (https://www.urbandictionary.com/yesterday.php) will contain it)
+end = 17968830
+total = 2000000 # the number of words to check (it counts backwards)
 
 name = "recent"
 # make dictionary an array of dictionaries
 dic = Manager().list()
 with Pool(20) as p:
-  for _ in tqdm.tqdm(p.imap_unordered(get, range(start, end)), total=end - start):
+  for _ in tqdm.tqdm(p.imap_unordered(get, range(1, total)), total=total):
     pass
 
 with open(str(name)+".json", "w", encoding="utf-8") as f:
